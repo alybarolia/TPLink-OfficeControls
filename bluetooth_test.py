753ae69c409a8650ea5277bc939690b8943e8c5a
Aly Khan Barolia <32456565+alybarolia@users.noreply.github.com>
2025-01-03 14:34:42 -0500

    Addition of config and Bluetooth functionality
    config file and Bluetooth module to allow on and off functionality for LED lights

---
+++ bluetooth_test.py
@@ -0,0 +1,123 @@
+
+import asyncio
+from bleak import BleakClient, BleakScanner
+#import config as cfg
+import json
+
+
+with open("config.json") as jsonfile:
+    data = json.load(jsonfile) # Reading the file
+    print("Read successful from bluetooth_test")
+    #print(data["light1"])
+
+
+characteristic_uuid = data["characteristic_uuid"]
+#bt_address = ''
+
+
+
+async def details():
+    # Device name to be searched for
+    my_device_name = data["btDeviceName"]
+    # Device Enumeration
+    devices = await BleakScanner.discover()
+    myDevice = None
+    for d in devices:
+        if d.name == my_device_name:
+            myDevice = d
+    if(myDevice != None):
+        # Device Connection
+        async with BleakClient(str(myDevice.address)) as client:
+            print('List of all characteristics: ')
+            for characteristic in client.services.characteristics:
+                x = client.services.characteristics.get(characteristic)
+                print(client.services.get_service(x.service_handle).description, x.service_uuid, 
+                       x.description, x.uuid, x.properties)
+#asyncio.run(details())
+
+
+
+# Bluetooth LE scanner
+async def connect():
+    myDevice = ''
+    devices = await BleakScanner.discover(5.0, return_adv=True)
+    for d in devices:
+        if(devices[d][1].local_name == data["btDeviceName"]):
+            print("Found BT device")
+            myDevice = d
+    ble_address = myDevice
+    print (ble_address)
+    data["ble_address"] = ble_address
+    with open("config.json", "w") as jsonFile:
+        json.dump(data, jsonFile)
+    global bt_address 
+    bt_address = ble_address
+    
+    
+
+   
+    ##    Service: 00001800-0000-1000-8000-00805f9b34fb (Generic Access Profile)
+    ##    Characteristic: 00002a00-0000-1000-8000-00805f9b34fb (Device Name)
+    ##    Service: 0000ffd5-0000-1000-8000-00805f9b34fb (Vendor specific)
+    ##    Characteristic: 0000ffda-0000-1000-8000-00805f9b34fb (Vendor specific)
+    ##    Characteristic: 0000ffd9-0000-1000-8000-00805f9b34fb (Vendor specific)
+    ##    Service: 0000ffd0-0000-1000-8000-00805f9b34fb (Vendor specific)
+    ##    Characteristic: 0000ffd4-0000-1000-8000-00805f9b34fb (Vendor specific)
+    ##    Characteristic: 0000ffd1-0000-1000-8000-00805f9b34fb (Vendor specific)
+
+    ##List of all characteristics: 
+    ##Generic Access Profile 00001800-0000-1000-8000-00805f9b34fb Device Name 00002a00-0000-1000-8000-00805f9b34fb ['read']
+    ##Vendor specific 0000ffd5-0000-1000-8000-00805f9b34fb Vendor specific 0000ffda-0000-1000-8000-00805f9b34fb ['notify']
+    ##Vendor specific 0000ffd5-0000-1000-8000-00805f9b34fb Vendor specific 0000ffd9-0000-1000-8000-00805f9b34fb ['write-without-response', 'write']
+    ##Vendor specific 0000ffd0-0000-1000-8000-00805f9b34fb Vendor specific 0000ffd4-0000-1000-8000-00805f9b34fb ['notify']
+    ##Vendor specific 0000ffd0-0000-1000-8000-00805f9b34fb Vendor specific 0000ffd1-0000-1000-8000-00805f9b34fb ['write-without-response']
+
+    async with BleakClient(ble_address) as client:
+        print("Connected to BLE device")
+        print(client.is_connected)
+        #await client.start_notify(characteristic_uuid, notification_handler)
+        #await asyncio.sleep(10.0)
+        #await client.stop_notify(characteristic_uuid)
+
+
+
+
+
+
+async def turn_on():
+    print(data["ble_address"])
+    async with BleakClient(data["ble_address"]) as client:
+        print("Connected to BLE device")
+        if (client.is_connected):
+            lista = [204, 35, 51]
+            values = bytearray(lista) 
+
+        await client.write_gatt_char(characteristic_uuid, values, False)
+    print("LED lights turned on")
+
+
+
+async def turn_off():
+    print(data["ble_address"])
+    async with BleakClient(data["ble_address"]) as client:
+        print("Connected to BLE device")
+        if (client.is_connected):
+            lista = [204, 36, 51]
+            values = bytearray(lista) 
+
+        await client.write_gatt_char(characteristic_uuid, values, False)
+
+        #data = await client.read_gatt_char(characteristic_uuid)
+        #print(data)
+    print("LED lights turned off")
+
+
+
+
+
+
+
+#asyncio.run(connect())
+#asyncio.run(turn_on())
+
+
